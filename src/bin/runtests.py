cb2fe93af0a9a9542f507b3d5378d4da65d360bf
Matthias Goerner <enischte@gmail.com>
2011-12-05 00:50:04 -0800

    Changing polynomial parser to a stack based parser for better performance.

---
+++ src/bin/runtests.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+import os
+import sys
+import re
+import optparse
+import traceback
+import StringIO
+import csv
+import doctest
+
+this_path, this_file = os.path.split(sys.argv[0])
+abs_path = os.path.abspath(this_path)
+base_path, this_dir = os.path.split(abs_path)
+sys.path.append(base_path)
+
+try:
+    import manifold.triangulation
+    import manifold.slN
+    import algebra.polynomial
+    import algebra.magma
+except ImportError as e:
+    print e
+    print
+    print "This program was called as       :", sys.argv[0]
+    print "Absolute path to this program is :", abs_path
+    print "Base path is                     :", base_path
+    sys.exit(1)
+
+doctest.testmod(manifold.triangulation)
+doctest.testmod(manifold.slN)
+doctest.testmod(algebra.polynomial)
+doctest.testmod(algebra.magma)
