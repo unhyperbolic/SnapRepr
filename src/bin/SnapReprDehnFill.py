db8cd86a63e7637f855109019dd80f2df45c9cfa
Matthias Goerner <enischte@gmail.com>
2012-02-03 14:22:35 -0800

    Adding to SnapReprDehnFill to use different value range for p and q

--- src/bin/SnapReprDehnFill.py
+++ src/bin/SnapReprDehnFill.py
@@ -6,21 +6,22 @@ import fractions
 
 import snappy
 
-if not len(sys.argv) == 4:
-    print "Usage: SnapReprDehnFill trig_file targetDir maxDehnFillCoefficient"
+if not len(sys.argv) == 5:
+    print "Usage: SnapReprDehnFill trig_file targetDir maxDehnFillCoefficientP maxDehnFillCoefficientQ"
     sys.exit(1)
 
-trig_file, targetDir, d = sys.argv[1:4]
+trig_file, targetDir, pDehn, qDehn = sys.argv[1:5]
 
-d = int(d)
+pDehn = int(pDehn)
+qDehn = int(qDehn)
 
 M = snappy.Manifold(trig_file)
 
 baseFile = targetDir + '/' + trig_file.split('/')[-1][:-5]
 
 for c in range(M.num_cusps()):
-    for i in range(-d, d+1):
-        for j in range(0, d+1):
+    for i in range(-pDehn, pDehn+1):
+        for j in range(0, qDehn+1):
             if j > 0 or i > 0:
                 if fractions.gcd(i,j) == 1:
                     M = snappy.Manifold(trig_file)
